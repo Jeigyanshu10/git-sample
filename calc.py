f7da380ecabbb11e3489af1adf296b84f1ef6906
Jeigyanshu Sarangi <Jeigyanshu.sarangi@gmail.com>
2019-07-13 09:42:22 +0000

    changed from divide to modulus

--- calc.py
+++ calc.py
@@ -2,10 +2,10 @@
 def add(x,y):
    return x+y
 def subtract(x,y):
-	retur x-y
+	return x-y
 def multiply(x,y):
 	return x*y
-def divide(x,y):
-	return x/y
+def modulus(x,y):
+	return x%y
 def square(x,y):
 	returne x^y
